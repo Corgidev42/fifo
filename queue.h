50b5db3c3ff619aa4e6fafded250f070755ce6d7
vbonnard <dev@MacBook-Pro-3.local>
2024-11-14 10:06:13 +0100

    c file and h file with png file

---
+++ queue.h
@@ -0,0 +1,36 @@
+#ifndef QUEUE_H
+# define QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Définition du type Booléen */
+typedef enum
+{
+	false,
+	true
+} Bool;
+
+/* Définition d'une File */
+typedef struct QueueElement
+{
+	int value;
+	struct QueueElement *next;
+} QueueElement, *Queue;
+
+/* Paramètres de la File */
+static QueueElement	*first = NULL;
+static QueueElement	*last = NULL;
+static int			nb_elements = 0;
+
+/* Prototypes */
+Bool	is_empty_queue(void);
+int		queue_length(void);
+int		queue_first(void);
+int		queue_last(void);
+void	print_queue(void);
+void	push_queue(int x);
+void	pop_queue(void);
+void	clear_queue(void);
+
+#endif
